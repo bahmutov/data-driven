78d20e7a705cc25427aab5cfdc07b2eb635f0c57
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-09-03 01:04:40 -0400

    initial

---
+++ src/data-driven-spec.js
@@ -0,0 +1,11 @@
+'use strict'
+
+
+/* eslint-env mocha */
+const dataDriven = require('.')
+
+describe('@bahmutov/data-driven', () => {
+  it('write this test', () => {
+    console.assert(dataDriven, 'should export something')
+  })
+})
